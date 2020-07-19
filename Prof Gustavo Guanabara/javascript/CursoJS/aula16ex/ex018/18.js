51e0cd054aaaf73d91fb36ef32c6b52062002001
fell0w-sch0l4r <angelcagiza@gmail.com>
2020-07-19 15:17:44 +0100

    teste

--- Prof Gustavo Guanabara/javascript/CursoJS/aula16ex/ex018/18.js
+++ Prof Gustavo Guanabara/javascript/CursoJS/aula16ex/ex018/18.js
@@ -3,7 +3,7 @@ var num = []
 function ad(){
     document.querySelector('div#res').innerHTML = ''
     
-    numero = document.querySelector('input#n1').value
+    var numero = document.querySelector('input#n1').value
     n1 = Number(document.querySelector('input#n1').value)
     if(numero.length===0 || n1<0 || n1>100 || num.indexOf(n1)!=-1){
         alert('ERRO! Valor inválido ou já encontrado na lista')
@@ -15,6 +15,8 @@ function ad(){
 
         document.getElementById('tab').appendChild(op)
     }
+
+    
 }
 
 
@@ -26,23 +28,23 @@ function fin(){
         alert('Adicione valores antes de finalizar.')
     }else{
         var resposta = document.querySelector('div#res')
-    resposta.innerHTML+= `<p>Ao todo, temos ${num.length} números cadastrados.</p>`
-    let maior = menor = soma = 0
-    for(let c in num){
-        if(c==='0'){
-            maior=menor=num[c]
-        }else{
-            if(num[c]>maior){
-                maior=num[c]
-            }else if(num[c]<menor){
-                menor=num[c]
+        resposta.innerHTML+= `<p>Ao todo, temos ${num.length} números cadastrados.</p>`
+        let maior = menor = soma = 0
+        for(let c in num){
+            if(c==='0'){
+                maior=menor=num[c]
+            }else{
+                if(num[c]>maior){
+                    maior=num[c]
+                }else if(num[c]<menor){
+                    menor=num[c]
+            }
+        }   soma += num[c]
         }
-    }   soma += num[c]
-    }
-    resposta.innerHTML+= `<p>O maior valor informado foi ${maior}</p>`
-    resposta.innerHTML+= `<p>O menor valor informado foi ${menor}</p>`
-    resposta.innerHTML+= `<p>Somando todos os valores temos ${soma}</p>`
-    resposta.innerHTML+= `<p>A média dos valores digitados é ${soma/num.length}</p>`
+        resposta.innerHTML+= `<p>O maior valor informado foi ${maior}</p>`
+        resposta.innerHTML+= `<p>O menor valor informado foi ${menor}</p>`
+        resposta.innerHTML+= `<p>Somando todos os valores temos ${soma}</p>`
+        resposta.innerHTML+= `<p>A média dos valores digitados é ${soma/num.length}</p>`
     }
 
     
@@ -50,20 +52,3 @@ function fin(){
 }
 
 
-
-let maior = menor = 0
-for(let c in num){
-    if(c==='0'){
-        maior=menor=num[c]
-    }else{
-        if(num[c]>maior){
-            maior=num[c]
-        }else if(num[c]<menor){
-            menor=num[c]
-        }
-    }
-}
-    
-console.log(maior)
-console.log(menor)
-console.log(typeof(num[5]))
